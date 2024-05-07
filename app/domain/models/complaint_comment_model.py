e5c9d69767a4deb2bb41b83b3359b5bd2edafdef
leningael <leningaelrs@gmail.com>
2024-05-07 16:07:30 -0600

    feat: :sparkles: Application models defined

---
+++ app/domain/models/complaint_comment_model.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from uuid import UUID
+
+from app.domain.models.user_model import UserModel
+
+
+class ComplaintCommentModel:
+    def __init__(
+        self,
+        id: UUID | None,
+        incident_id: UUID,
+        user_id: UUID,
+        content: str,
+        date: datetime,
+        image_url: str,
+        user: UserModel | None = None
+    ):
+        self.id = id
+        self.incident_id = incident_id
+        self.user_id = user_id
+        self.content = content
+        self.date = date
+        self.image_url = image_url
+        self.user = user
