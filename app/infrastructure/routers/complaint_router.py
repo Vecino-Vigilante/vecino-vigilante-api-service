fba7db780c0229e3c429011e91afdbd9c3d595c0
leningael <leningaelrs@gmail.com>
2024-05-08 11:26:28 -0600

    docs: :memo: Return types added to be documented in docs

--- app/infrastructure/routers/complaint_router.py
+++ app/infrastructure/routers/complaint_router.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.application.services.complaint_service import ComplaintsService
 from app.domain.exceptions.resource_not_found_exception import ResourceNotFoundException
-from app.infrastructure.dto.complaint_dto import ComplaintRequestDTO
+from app.infrastructure.dto.complaint_dto import ComplaintDTO, ComplaintDetailDTO, ComplaintRequestDTO
 from app.infrastructure.mappers.complaint_mappers import (
     map_complaint_model_to_complaint_detail_dto,
     map_complaint_model_to_complaint_dto,
@@ -27,7 +27,7 @@ complaint_service = ComplaintsService(
 
 
 @complaint_router.post("", status_code=status.HTTP_201_CREATED)
-def create_complaint(complaint: ComplaintRequestDTO):
+def create_complaint(complaint: ComplaintRequestDTO) -> ComplaintDTO:
     new_complaint = complaint_service.create_complaint(
         map_complaint_req_dto_to_complaint_model(complaint), complaint.resource
     )
@@ -39,7 +39,7 @@ def get_complaints(
     start_date: datetime | None = None,
     end_date: datetime | None = None,
     type_id: UUID | None = None,
-):
+) -> list[ComplaintDTO]:
     return [
         map_complaint_model_to_complaint_dto(complaint)
         for complaint in complaint_service.get_complaints(start_date, end_date, type_id)
@@ -47,7 +47,7 @@ def get_complaints(
 
 
 @complaint_router.get("/{incident_id}")
-def get_complaint_by_id(incident_id: UUID):
+def get_complaint_by_id(incident_id: UUID) -> ComplaintDetailDTO:
     try:
         return map_complaint_model_to_complaint_detail_dto(
             complaint_service.get_complaint_by_id(incident_id)
@@ -59,7 +59,7 @@ def get_complaint_by_id(incident_id: UUID):
         )
 
 @complaint_router.put("/{incident_id}")
-def update_complaint(incident_id: UUID, complaint: ComplaintRequestDTO):
+def update_complaint(incident_id: UUID, complaint: ComplaintRequestDTO) -> ComplaintDTO:
     try:
         complaint_model = map_complaint_req_dto_to_complaint_model(complaint)
         complaint_model.id = incident_id
@@ -73,7 +73,7 @@ def update_complaint(incident_id: UUID, complaint: ComplaintRequestDTO):
         )
         
 @complaint_router.delete("/{incident_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_complaint(incident_id: UUID):
+def delete_complaint(incident_id: UUID) -> None:
     try:
         complaint_service.delete_complaint(incident_id)
     except ResourceNotFoundException:
